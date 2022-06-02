b885dde3c517bbae86822566efb18b6dcdb9d645
C.Y <105695766+Gicheh@users.noreply.github.com>
2022-06-02 08:21:52 +0300

    Add files via upload

---
+++ instances.py
@@ -0,0 +1,9 @@
+from traceback import print_tb
+from schools import School
+
+
+school_one=School(414,"Nairobi")
+school_two=School(562,"Mombasa")
+school_three=School(234,"Kisumu")
+
+print(school_one.no_of_students,school_one.location)
